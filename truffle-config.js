39dc0ec613e5e92ef816fe02c8de6387c369daf3
George Tzortzakis <georgetzortzakis@yahoo.com>
2022-10-19 02:57:19 +0300

    Configured Truffle-config.js compiled KryptoBird created .json for KryptoBird

--- truffle-config.js
+++ truffle-config.js
@@ -47,28 +47,19 @@
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a managed Ganache instance for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+  
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache, geth, or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+   
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+    },
+
+     contracts_directory: './src/contracts/',
+     contracts_build_directory: './src/abis'
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -96,23 +87,21 @@ module.exports = {
     //   network_id: 2111,   // This network is yours, in the cloud.
     //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+  ,
 
   // Set default mocha options here, use special reporters, etc.
-  mocha: {
-    // timeout: 100000
-  },
-
+ 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.8.0",
+      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       optimizer: {
+          enabled: false,
+          runs: 200
+        },
       //  evmVersion: "byzantium"
       // }
     }
