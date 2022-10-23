303cbf74af83c79e6dd0d079565f3d77f26675e4
George Tzortzakis <georgetzortzakis@yahoo.com>
2022-10-23 20:49:42 +0300

    Added ownerbyIndex functionality and Token by index

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -24,12 +24,30 @@ contract ERC721Enunerable is ERC721 {
         // A. add tokens to the owner
         // B. add tokens to our totalsupply - to allTokens
 
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
-        _allTokens.push(tokenId); //we are going to push the function to _mint function
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+
+        _allTokensIndex[tokenId] = _allTokens.length;  //token index receives the value of the position in the array
+       _allTokens.push(tokenId); //we are going to push the function to _mint function
         //to keep track of tokens added.
+    }
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+           _ownedTokens[to].push(tokenId);
+           _ownedTokensIndex[tokenId]= _ownedTokens[to].length;
+    }
+
+    function tokenByIndex(uint256 index) public view returns(uint256) {
+            require(index < totalSupply(), 'global index beyond total supply!');
+            return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address _owner, uint256 index) public view returns(uint256) {
+        require(balanceOf(_owner) > index, 'owner index out of bounds');
+        return _ownedTokens[_owner][index];
     }
 
     function totalSupply() public view returns(uint) {
